bada022dd75ea995e77538e670aa1a18e70b5201
josephramon <44426022+josephramon@users.noreply.github.com>
2021-12-31 21:57:37 +0800

    sql - create sample database and table

--- mysql/python_mysql.sql
+++ mysql/python_mysql.sql
@@ -73,3 +73,4 @@ insert  into `books`(`id`,`title`,`isbn`) values (1,'Bel and the Dragon ','12382
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
